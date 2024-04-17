0244d139b7f9a92d0b5edb7de32b2c4ff04e174d
caiorr1 <caiorrodrigues2004@gmail.com>
2024-04-17 15:20:49 -0300

    feat: adicionando primeiros componentes ao app.tsx

--- App.tsx
+++ App.tsx
@@ -3,3 +3,15 @@ import { NativeBaseProvider, View } from 'native-base';
 import AdicionarTarefa from "./src/components/AdicionarTarefa";
 import { ProvedorEstadoGlobal } from "./src/hooks/EstadoGlobal";
 import ListaTarefas from "./src/components/ListaTarefas";
+
+export default function App() {
+  return (
+    
+    <NativeBaseProvider>
+      <ProvedorEstadoGlobal>
+       
+      </ProvedorEstadoGlobal>
+    </NativeBaseProvider>
+
+  );
+}
